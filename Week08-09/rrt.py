ab19859af568680bd6fc39c743e9223d0f259d8e
Dylan Gonzalez <dylcg10@gmail.com>
2023-09-18 15:15:32 +1000

    fixed indentation errors

--- Week08-09/rrt.py
+++ Week08-09/rrt.py
@@ -1,190 +1,16 @@
-# M4 - Autonomous fruit searching
+'''
+MIT License
+Copyright (c) 2019 Fanjin Zeng
+This work is licensed under the terms of the MIT license, see <https://opensource.org/licenses/MIT>.  
 
-# basic python packages
-import sys, os
-import cv2
+Test file for viewing RRT implementation
+'''
+
 import numpy as np
-import json
-import argparse
-import time
-import matplotlib.pyplot as plt
-from matplotlib import collections as mc
-from collections import deque
 from random import random
-
-
-# import SLAM components
-# sys.path.insert(0, "{}/slam".format(os.getcwd()))
-# from slam.ekf import EKF
-# from slam.robot import Robot
-# import slam.aruco_detector as aruco
-
-# import utility functions
-sys.path.insert(0, "util")
-from pibot import PenguinPi
-import measure as measure
-
-
-def read_true_map(fname):
-    """Read the ground truth map and output the pose of the ArUco markers and 5 target fruits&vegs to search for
-
-    @param fname: filename of the map
-    @return:
-        1) list of targets, e.g. ['lemon', 'tomato', 'garlic']
-        2) locations of the targets, [[x1, y1], ..... [xn, yn]]
-        3) locations of ArUco markers in order, i.e. pos[9, :] = position of the aruco10_0 marker
-    """
-    with open(fname, 'r') as fd:
-        gt_dict = json.load(fd)
-        fruit_list = []
-        fruit_true_pos = []
-        aruco_true_pos = np.empty([10, 2])
-
-        # remove unique id of targets of the same type
-        for key in gt_dict:
-            x = np.round(gt_dict[key]['x'], 1)
-            y = np.round(gt_dict[key]['y'], 1)
-
-            if key.startswith('aruco'):
-                if key.startswith('aruco10'):
-                    aruco_true_pos[9][0] = x
-                    aruco_true_pos[9][1] = y
-                else:
-                    marker_id = int(key[5]) - 1
-                    aruco_true_pos[marker_id][0] = x
-                    aruco_true_pos[marker_id][1] = y
-            else:
-                fruit_list.append(key[:-2])
-                if len(fruit_true_pos) == 0:
-                    fruit_true_pos = np.array([[x, y]])
-                else:
-                    fruit_true_pos = np.append(fruit_true_pos, [[x, y]], axis=0)
-
-        #return (1), (2), (3)
-        return fruit_list, fruit_true_pos, aruco_true_pos
-
-
-def read_search_list():
-    """Read the search order of the target fruits
-
-    @return: search order of the target fruits
-    """
-    search_list = []
-    with open('search_list.txt', 'r') as fd:
-        fruits = fd.readlines()
-
-        for fruit in fruits:
-            search_list.append(fruit.strip())
-
-    return search_list
-
-
-def print_target_fruits_pos(search_list, fruit_list, fruit_true_pos):
-    """Print out the target fruits' pos in the search order
-
-    @param search_list: search order of the fruits
-    @param fruit_list: list of target fruits
-    @param fruit_true_pos: positions of the target fruits
-    """
-
-    print("Search order:")
-    n_fruit = 1
-    for fruit in search_list:
-        for i in range(3):
-            if fruit == fruit_list[i]:
-                print('{}) {} at [{}, {}]'.format(n_fruit,
-                                                  fruit,
-                                                  np.round(fruit_true_pos[i][0], 1),
-                                                  np.round(fruit_true_pos[i][1], 1)))
-        n_fruit += 1
-
-
-# Waypoint navigation
-# the robot automatically drives to a given [x,y] coordinate
-# note that this function requires your camera and wheel calibration parameters from M2, and the "util" folder from M1
-# fully automatic navigation:
-# try developing a path-finding algorithm that produces the waypoints automatically
-import math
-import time
-
-def drive_to_point(waypoint, robot_pose):
-    try:
-        # ... (initialize parameters and calibration data)
-
-        wheel_vel = 30  # tick
-        correction_gain = 0.1  # Adjust as needed
-        desired_radius = 0.5
-                    
-        while True:
-            current_angle = robot_pose[2]
-            target_angle = math.atan2(waypoint[1] - robot_pose[1], waypoint[0] - robot_pose[0])
-            angle_to_turn = target_angle - current_angle
-
-
-            # Ensure the turn angle is within the range of -π to π (or -180 degrees to 180 degrees)
-            if angle_to_turn >= math.pi:
-                angle_to_turn -= 2 * math.pi
-                turn_direction = -1
-            elif angle_to_turn < -math.pi:
-                angle_to_turn += 2 * math.pi
-                turn_direction = 1
-
-            turn_time = abs(angle_to_turn) / wheel_vel
-            print("Turning for {:.2f} seconds".format(turn_time))
-            ppi.set_velocity([0, turn_direction], turning_tick=wheel_vel, time=turn_time)
-
-            # Drive straight to the waypoint
-            distance_to_waypoint = math.sqrt((waypoint[0] - robot_pose[0])**2 + (waypoint[1] - robot_pose[1])**2)
-            drive_time = distance_to_waypoint / wheel_vel
-
-            print("Driving for {:.2f} seconds".format(drive_time))
-            ppi.set_velocity([1, 0], tick=wheel_vel, time=drive_time)
-
-            # Update robot pose based on sensor feedback
-            robot_pose = get_robot_pose()
-
-            # Check if the robot is within the desired radius of the waypoint
-            if distance_to_waypoint <= desired_radius:
-                #implement the delay of 2 seconds
-                break  # Stop the robot
-
-            # MODIFY: Sleep to control the update rate
-            time.sleep(0.1)
-
-    except Exception as e:
-        # Handle exceptions gracefully
-        print("An error occurred:", str(e))
-    finally:
-        # Implement a way to gracefully terminate the navigation process if needed
-        ppi.stop()  # Stop the robot
-
-    print("Arrived at [{}, {}]".format(waypoint[0], waypoint[1]))
-
-
-def get_robot_pose():
-    ####################################################
-    # TODO: replace with your codes to estimate the pose of the robot
-    # We STRONGLY RECOMMEND you to use your SLAM code from M2 here
-
-    # update the robot pose [x,y,theta]
-    robot_pose = [0.0,0.0,0.0] # replace with your calculation
-    ####################################################
-
-    return robot_pose
-
-class Node():
-    """A node class for A* Pathfinding"""
-
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
+import matplotlib.pyplot as plt
+from matplotlib import collections  as mc
+from collections import deque
 
 class Line():
     ''' Define line '''
@@ -216,14 +42,18 @@ def Intersection(line, center, radius):
 
     return True
 
+
+
 def distance(x, y):
     return np.linalg.norm(np.array(x) - np.array(y))
+
 
 def isInObstacle(vex, obstacles, radius):
     for obs in obstacles:
         if distance(obs, vex) < radius:
             return True
     return False
+
 
 def isThruObstacle(line, obstacles, radius):
     for obs in obstacles:
@@ -279,7 +109,7 @@ def isInWindow(pos, winx, winy, width, height):
 
 
 class Graph:
-''' Define graph '''
+    ''' Define graph   '''
     def __init__(self, startpos, endpos):
         self.startpos = startpos
         self.endpos = endpos
@@ -405,7 +235,7 @@ def RRT_star(startpos, endpos, obstacles, n_iter, radius, stepSize):
 
 def dijkstra(G):
     '''
-    dijkstra algorithm for finding shortest path from start position to end.
+    Dijkstra algorithm for finding shortest path from start position to end.
     '''
     srcIdx = G.vex2idx[G.startpos]
     dstIdx = G.vex2idx[G.endpos]
@@ -476,40 +306,11 @@ def pathSearch(startpos, endpos, obstacles, n_iter, radius, stepSize):
         # plot(G, obstacles, radius, path)
         return path
 
-# main loop
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Fruit searching")
-    parser.add_argument("--map", type=str, default='M4_true_map_full.txt') # change to 'M4_true_map_part.txt' for lv2&3
-    parser.add_argument("--ip", metavar='', type=str, default='192.168.50.1')
-    parser.add_argument("--port", metavar='', type=int, default=8080)
-    args, _ = parser.parse_known_args()
 
-    ppi = PenguinPi(args.ip,args.port)
-
-    # read in the true map
-    fruits_list, fruits_true_pos, aruco_true_pos = read_true_map(args.map) #list of fruits names, locations of fruits, locations of aruco markers
-    search_list = read_search_list() #inputted ordered list of fruits to search 
-    print_target_fruits_pos(search_list, fruits_list, fruits_true_pos)
-
-    waypoint = [0.0,0.0]
-    robot_pose = [0.0,0.0,0.0]
-
-    path_planner()
-
-
-    # ---- We won't need the skeleton code below ----# 
-    # ---- Use RRT* + Djikstra's instead ----#
+if __name__ ==   '__main__':
     startpos = (0., 0.)
-
-    end_target = search_list[-1]
-    for target in fruits_true_pos:
-        if target[0] == end_target:
-            endpos = target[1]
-            break
-
-    #obstacles = fruits and markers
-    obstacles = np.concatenate(fruits_true_pos, aruco_true_pos)
-
+    endpos = (5., 5.)
+    obstacles = [(1., 1.), (2., 2.)]
     n_iter = 200
     radius = 0.5
     stepSize = 0.7
@@ -524,11 +325,4 @@ if __name__ == "__main__":
     else:
         plot(G, obstacles, radius)
 
-   for point in path:
-       robot_pose = get_robot_pose()
-       if np.linalg.norm(robot_pose, endpos) <= radius:
-           break
 
-       drive_to_point(point, robot_pose)
-       print("Finished driving to waypoint: {}; New robot pose: {}".format(waypoint,robot_pose))
-       ppi.set_velocity([0,0])
